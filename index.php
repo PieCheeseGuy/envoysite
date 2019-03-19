9da1481185e8fe94f40353e5103d92eb1fdd631b
ctOS <andrew.cao.mn@gmail.com>
2019-03-19 08:03:39 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-
+<!DOCTYPE html!>
+<html>
+<header>
+  <title>Issa Mohamed</title>
+</header>
+  
+<body>
+<br>
+  <p><h1><center>Photos that Come to Life</center></h1></p>
+<br>
+  
+</body>
+  
+</html>
